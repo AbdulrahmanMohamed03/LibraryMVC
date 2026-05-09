52dc76b7f3106c1691eb2be58a65a2094f6c6d03
AbdelrahmanMohamed03 <podap384@gmail.com>
2026-05-09 22:28:59 +0300

    fix:admin managing roles

---
+++ Project.Application/ViewModels/Role/ManageUserRolesVm.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Project.Application.ViewModels.Role
+{
+    public class ManageUserRolesVm
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+        public List<UserRoleVm> Roles { get; set; } = new();
+        [Required(ErrorMessage = "Please select a role")]
+        public string SelectedRole { get; set; }
+        public List<SelectListItem> AllRoles { get; set; } = new();
+    }
+}
