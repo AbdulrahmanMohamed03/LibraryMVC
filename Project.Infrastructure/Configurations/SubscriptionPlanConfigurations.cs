ceca4adecd17c3847051fe37841fa6eac1e1e214
AbdelrahmanMohamed03 <podap384@gmail.com>
2026-05-11 10:42:24 +0300

    fix: migration added to fix reservation status

---
+++ Project.Infrastructure/Configurations/SubscriptionPlanConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Infrastructure.Configurations
+{
+    public class SubscriptionPlanConfigurations : IEntityTypeConfiguration<SubscriptionPlan>
+    {
+        public void Configure(EntityTypeBuilder<SubscriptionPlan> builder)
+        {
+            builder.Property(x => x.MonthlyFee)
+                   .HasPrecision(18, 2);
+        }
+    }
+}
